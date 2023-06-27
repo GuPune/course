7f904fab1031f15b20a04f81c18bd794dc27fa81
Rkknoob <iboonkhet@gmail.com>
2023-06-27 22:16:41 +0700

    update
    update

---
+++ store/modules/user.js
@@ -0,0 +1,45 @@
+
+
+import {
+  
+
+} from "../actions.type.js";
+import {
+    
+
+} from "../mutations.type.js";
+import Vuex from 'vuex'
+
+
+const state = {
+    typeforgot:null,
+
+};
+
+const getters = {
+    typeforgot: state => {
+        return state.typeforgot
+    },
+   
+
+};
+
+const actions = {
+ 
+  
+ 
+
+    
+
+};
+
+const mutations = {
+ 
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
