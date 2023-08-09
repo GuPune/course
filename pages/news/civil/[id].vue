6ff35ecf0832ff1756e5dfd91fa0900de00ef048
iboonkhet@gmail.com <123219570+Rkknoob1@users.noreply.github.com>
2023-08-09 17:39:48 +0700

    update
    update

--- pages/news/civil/[id].vue
+++ pages/news/civil/[id].vue
@@ -46,114 +46,84 @@
                                 <img src="../../../assets/img/blog/blog_21.png" alt="blog">
                             </div>
                             <div class="blog__details__content">
-                                <p class="content__1" data-aos="fade-up">We have covered many special events such as
-                                    fireworks, fairs, parades, races, walks, awards ceremonies, fashion shows, sporting
-                                    events, and even a memorial service.
-                                </p>
-                                <p class="content__2" data-aos="fade-up">Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit. Curabitur vulputate vestibulum rhoncus, dolor eget viverra pretium, dolor tellus
-                                    aliquet nunc, vitae ultricies erat elit eu lacus. Vestibulum non justo consectetur,
-                                    cursus ante, tincidunt sapien. Nulla quis diam sit amet turpis interd enim. Vivamus
-                                    faucibus ex sed nibh egestas elementum. Mauris et bibendum dui. Aenean consequat
-                                    pulvinar luctus. Suspendisse consectetur tristiquexxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                                    tortor
-                                </p>
                                 <div class="blog__details__heading" data-aos="fade-up">
-                                    <h5>Experience is over the world visit</h5>
+                                    <h5>{{ store.newscivilone.news_title }}</h5>
                                 </div>
-                                <p class="content__3" data-aos="fade-up">Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit. Curabitur vulputate vestibulum Phasellus rhoncus, dolor eget viverra pretium,
-                                    dolor tellus aliquet nunc, vitae ultricies erat elit eu lacus. Vestibulum non justo
-                                    consectetur, cursus ante, tincidunt sapien. Nulla quis diam sit amet turpis interdum
-                                    accumsan quis nec enim. Vivamus faucibus ex sed nibh egestas elementum. Mauris et
-                                    bibendum dui. Aenean consequat pulvinar luctus</p>
-                                <p class="content__4" data-aos="fade-up">We have covered many special events such as
-                                    fireworks, fairs, parades, races, walks, awards ceremonies, fashion shows, sporting
-                                    events, and even a memorial service.
+
+
+                                <p class="content__7" data-aos="fade-up">
+
+                                    {{ store.newscivilone.news_description }}
                                 </p>
-                                <p class="content__5" data-aos="fade-up">Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit. Curabitur vulputate vestibulum Phasellus rhoncus, dolor eget viverra pretium,
-                                    dolor tellus aliquet nunc, vitae ultricies erat elit eu lacus. Vestibulum non justo
-                                    consectetur, cursus ante, tincidunt sapien. Nulla quis diam sit amet turpis interdum
-                                    accumsan quis nec enim. Vivamus faucibus ex sed nibh egestas elementum. Mauris et
-                                    bibendum dui. Aenean consequat pulvinar luctus.
-                                    Suspendisse consectetur tristique tortor</p>
-                                <div class="blockquate__wraper" data-aos="fade-up">
-                                    <div class="blockquate__content">
-                                        <h3>Smashing Podcast Episode With Paul Boag What Is Conversion Optimization</h3>
-                                        <a href="#">John Mirnsdo</a>
-                                    </div>
-                                    <div class="blockquate__img">
-                                        <img src="../../../assets/img/blog/blog_22.png" alt="img">
-                                    </div>
-
-                                </div>
-
-
-                                <div class="blog__details__heading" data-aos="fade-up">
-                                    <h5>Experience is over the world visit</h5>
-                                </div>
-
-                                <p class="content__6" data-aos="fade-up">We have covered many special events such as
-                                    fireworks, fairs, parades, races, walks, awards ceremonies, fashion shows, sporting
-                                    events, and even a memorial service</p>
-                                <p class="content__7" data-aos="fade-up">Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit. Curabitur vulputate vestibulum Phasellus rhoncus, dolor eget viverra pretium,
-                                    dolor tellus aliquet nunc, vitae ultricies erat elit eu lacus. Vestibulum non justo
-                                    consectetur, cursus ante, tincidunt sapien. Nulla quis diam sit amet turpis interdum
-                                    accumsan quis nec enim. Vivamus faucibus ex sed nibh egestas elementum. Mauris et
-                                    bibendum dui. Aenean consequat pulvinar luctus.
-                                    Suspendisse consectetur tristique tortor</p>
-
-                                <div class="blog__details__heading__2" data-aos="fade-up">
-                                    <h5>Let our investment management team</h5>
-                                </div>
-                                <p class="content__8" data-aos="fade-up">We have covered many special events such as
-                                    fireworks, fairs, parades, races, walks, awards ceremonies, fashion shows, sporting
-                                    events, and even a memorial service.
-                                </p>
-                                <p class="content__9" data-aos="fade-up">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vulputate vestibulum
-                                    Phasellus rhoncus, dolor eget viverra pretium, dolor tellus aliquet nunc, vitae
-                                    ultricies erat elit eu lacus. Vestibulum non justo consectetur, cursus ante, tincidunt
-                                    sapien. Nulla quis diam sit amet turpis interdum accumsan quis nec enim. Vivamus
-                                    faucibus ex sed nibh egestas elementum. Mauris et bibendum dui. Aenean consequat
-                                    pulvinar luctus. Suspendisse consectetur tristique tortor
-                                </p>
-
                             </div>
 
-
-
-
-
-
-
-                    </div>
-                </div>
-
-                <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
-
-                    <div class="blogsidebar__content__wraper__2" data-aos="fade-up">
-
-                        <h4 class="sidebar__title">categories</h4>
-                        <ul class="categorie__list">
-                            <li>
-                                <NuxtLink to="/news/civil">Civil Set <span>03</span></NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/news/transport">Transport Set <span>03</span></NuxtLink>
-                            </li>
-                        </ul>
-
+                        </div>
                     </div>
 
+                    <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+
+                        <div class="blogsidebar__content__wraper__2" data-aos="fade-up">
+
+                            <h4 class="sidebar__title">categories</h4>
+                            <ul class="categorie__list">
+                                <li>
+                                    <NuxtLink to="/news/transport">Transport Set<span>{{ store.newstranscount }}</span>
+                                    </NuxtLink>
+                                </li>
+                                <li>
+                                    <NuxtLink to="/news/civil">Civil Set <span>{{ store.newscivilcount }}</span></NuxtLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
 
                 </div>
-
             </div>
         </div>
-    </div>
 
-</div></template>
+    </div>
+</template>
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia';
+import { defineComponent } from 'vue';
+import { newsPostStore } from '@/stores/news';
+import { useRoute } from "vue-router";
+const store = newsPostStore()
+const router = useRouter();
+const route = useRoute();
+const { getisActiveNews } = storeToRefs(store);
+const { getselectNews } = storeToRefs(store);
+
+const { FetchNesTrans } = newsPostStore();//Action
+const { FetchNewsCivilId } = newsPostStore();//Action
+const { toggleActiveClassselect } = newsPostStore();
+
+await store.FetchNesTrans();
+await store.FetchNesCivil();
+await store.FetchNewsCivilId(route.params.id);
+
+
+
+
+async function fill(x) {
+    const formnew = reactive({
+        selectnew: x,
+    });
+    await toggleActiveClassselect(formnew);
+}
+
+function image(i) {
+    var x = null;
+    if (i) {
+        const usingSplit = i.split(',');
+        var x = usingSplit[0];
+    } else {
+        var x = 'static/upload/2023/7/files-1689561047889.jpg';
+    }
+    return "http://oasapi.iddriver.com/media_file/file/?f=" + x;
+}
+
+
+
+</script>
 <style scoped></style>
--- stores/news.ts
+++ stores/news.ts
@@ -11,6 +11,8 @@ export const newsPostStore = defineStore({
     isActiveNews:true,
     selectnew:"t",
     newstran:[],
+    newstranone:[],
+    newscivilone:[],
     newscivil:[],
     newstranscount:null,
     newscivilcount:null,
@@ -57,12 +59,28 @@ export const newsPostStore = defineStore({
         this.newscivilcount = response.data.data.length
         return true;
       });
+    },
+    async FetchNewTransId(id) {
+
+    for (var i = 0, len = this.newstran.length; i < len; i++) {
+      if(this.newstran[i].new_id = id){
+        this.newstranone = this.newstran[i]
+        break;
+      }
+  } 
+
+    },
+    async FetchNewsCivilId(id) {
+      for (var i = 0, len = this.newscivil.length; i < len; i++) {
+        if(this.newscivil[i].new_id = id){
+          this.newscivilone = this.newscivil[i]
+          break;
+        }
+    } 
+    
     }
 
-    
 
-    
-  
   }
 })
 
