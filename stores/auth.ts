a49254415496fd7e67f271a0c47b4953118604e2
Rkknoob <iboonkhet@gmail.com>
2023-08-29 00:54:23 +0700

    Update auth.ts

--- stores/auth.ts
+++ stores/auth.ts
@@ -57,22 +57,8 @@ export const useAuthStore = defineStore('auth', {
           const user_id = useCookie('user_id'); // useCookie new hook in nuxt 3
           token.value = "ZeBuphebrltl3uthIFraspubroST80Atr9tHuw5bODowi26p"; // set token to cookie
           user_id.value = data.value.user_id; // set token to cookie
-       //   localStorage.setItem('user', JSON.stringify(data.value))
-          // token.value = data?.value?.token; // set token to cookie
           this.authenticated = true; // set authenticated  state value to true
           this.status_login = true;
-        const checkveri = await ApiService.get('/user/get/'+data.value.user_id);
-        console.log(checkveri.data)
-        // const data = await ApiService.get('/user/get/90').then(response => {
-        
-          //   console.log(Object.keys(response.data.detail).length);
-       
-          // });
-          // if(Object.keys(response.data.detail).length === 0){
-         
-          // }else {
-           
-          // }
           return true;
         }else{
           this.status_login = false;
@@ -94,9 +80,12 @@ export const useAuthStore = defineStore('auth', {
       }
     },
     logUserOut() {
-      const token = useCookie('token'); // useCookie new hook in nuxt 3
-      this.authenticated = false; // set authenticated  state value to false
+      const token = useCookie('token'); 
+      const user_id = useCookie('user_id'); 
+      this.authenticated = false; 
+      this.verify = false; 
       token.value = null; // clear the token cookie
+      user_id.value = null;
       this.loading = false;
     },
   },
