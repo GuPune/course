352b8baa6378c2264d7386937b2b399eaa62fbdc
Rkknoob <iboonkhet@gmail.com>
2023-09-20 23:22:25 +0700

    update
    update

--- pages/exam/index.vue
+++ pages/exam/index.vue
@@ -39,7 +39,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-xl-12">
-                    <ExamFitter></ExamFitter>
+                    <!-- <ExamFitter></ExamFitter> -->
                     </div>
                     <div class="col-xl-12 col-lg-12 col-md-12 col-12">
                         <Examlist></Examlist>
