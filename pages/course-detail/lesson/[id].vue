45ebba1d8a0d21c491598c8984d3305a8b5158c7
iboonkhet <iboonkhet@gmail.com>
2023-12-25 09:57:54 +0700

    update
    update

--- pages/course-detail/lesson/[id].vue
+++ pages/course-detail/lesson/[id].vue
@@ -3,37 +3,22 @@
 
 <template>
   <div v-if="store.data.course_id">
-    <div >
+    <div>
       <div class="container">
         <div class="row" v-if="store.data">
           <div class="col-xl-12 col-lg-12">
             <div
               class="blogarae__img__2 course__details__img__2 aos-init aos-animate"
               data-aos="fade-up"
-            >
-              <!-- <img :src="coverimage(store.data.cs_cover)" alt="blog"> -->
-            </div>
+            ></div>
             <div class="blog__details__content__wraper">
               <div
                 class="course__button__wraper aos-init aos-animate"
                 data-aos="fade-up"
               >
                 <div class="course__button"></div>
-            
               </div>
-              <!-- <div
-                class="course__details__heading aos-init aos-animate"
-                data-aos="fade-up"
-              >
-                <h3>{{ store.data.cs_description }}</h3>
-              </div> -->
-              <!-- <div
-                class="course__details__price aos-init aos-animate"
-                data-aos="fade-up"
-              >
-                
-              </div> -->
-        
+
               <div
                 class="course__details__tab__wrapper aos-init"
                 data-aos="fade-up"
@@ -49,10 +34,9 @@
                           type="button"
                           aria-selected="true"
                           role="tab"
-                         
-                          @click="backlearning(store.data.course_id)" 
+                          @click="backlearning(store.data.course_id)"
                         >
-                         บทสู่บทเรียนหลัก
+                        {{ $t("backtocourse") }}
                         </button>
                       </li>
                     </ul>
@@ -67,9 +51,7 @@
                     id="projects__two"
                     role="tabpanel"
                     aria-labelledby="projects__two"
-                 
                   >
-           
                     <div
                       class="accordion content__cirriculum__wrap"
                       id="accordionExample"
@@ -78,10 +60,10 @@
                         <h2 class="accordion-header" id="headingOne">
                           <button
                             class="accordion-button collapsed mt-0"
-                            type="button" style="background-color:#7e7ec3"
-                        
+                            type="button"
+                            style="background-color: #7e7ec3"
                           >
-                          ບົດຮຽນ 
+                            ບົດຮຽນ
                           </button>
                         </h2>
                         <div
@@ -90,116 +72,95 @@
                           aria-labelledby="headingOne"
                           data-bs-parent="#accordionExample"
                         >
-                          <div class="row justify-content-center" v-if="store.data.cs_video">
-                            <!-- <vue3VideoPlay
-                              width="100%"
-                              title="Video"
-                             :src="store.data.cs_video"
-                              :poster="options.poster"
-                                     @pause="onPause"
-                              @timeupdate="onTimeupdate"
-                              @canplay="onCanplay"
-                             
-                            /> -->
-
-                             <!-- <video controls @play="onPlay(store.data.cs_id)">
-
-      <source :src="store.data.cs_video" type="video/mp4">
-      Your browser does not support the video tag.
-    </video> -->
-  
-   <div v-if="store.isYoutube == true" class="ifra">
-    <YouTube :src="store.data.cs_video" :width="store.windowWidth" @ready="onReady" @playing="handlePlaying" ref="youtube" />
-   </div>
-    
-                            <!-- <vue3VideoPlay
-                              width="100%"
-                              title="Video"
-                              :src="x.cs_video"
-                              :poster="options.poster"
-                              @play="onPlay(x.cs_id)"
-                              @pause="onPause"
-                              @timeupdate="onTimeupdate"
-                              @canplay="onCanplay"
-                            /> -->
-                          </div>
-
-                             <div class="row" style="padding: 5px">
-                                      <div class="single__expart__teacher">
-                                <div class="teacher__img mb-0" v-if="store.data.cs_cover">
-                              
-                                  <img
-                                    :src="coverimage(store.data.cs_cover)"
-                                    alt="Image"
-                                    style="width: 120px; height: 120px"
-                                  />
-                                </div>
-                              </div>
-                             </div>
+                   
 
                           <div class="row" style="padding: 5px">
-                            <!-- <div
-                              class="col-lg-3 d-flex justify-content-center align-items-center"
-                            >
-                              <div class="single__expart__teacher">
-                                <div class="teacher__img mb-0" v-if="store.data.cs_cover">
-                              
-                                  <img
-                                    :src="coverimage(store.data.cs_cover)"
-                                    alt="Image"
-                                    style="width: 120px; height: 120px"
-                                  />
-                                </div>
+                            <div class="single__expart__teacher">
+                              <div
+                                class="teacher__img mb-0"
+                                v-if="store.data.cs_cover"
+                              >
+                                <img
+                                  :src="coverimage(store.data.cs_cover)"
+                                  alt="Image"
+                                  class="responsive"
+                                />
                               </div>
-                            </div> -->
+                            </div>
+                          </div>
+
+                          <div class="row" style="padding: 5px">
+                          
                             <div class="col-lg-12 col--30">
                               <div
                                 class="author__content"
                                 style="margin: 30px 0"
                               >
-                                <!-- <div class="author__text row">
-                                  <div class="col-lg-3 col-md-4">
-                                    <p class="fs-4 fw-bold mb-0">
-                                      {{ $t("page_course_last_lesson") }} 
-                                    </p>
-                                  </div>
-                                  <div class="col-lg-9 col-md-8">
-                                    <p class="fs-4 fw-bold mb-0">
-                                        {{store.data.cs_name}}
-                                    </p>
-                                  </div>
-                                </div> -->
-                        
+                             
+
                                 <hr />
-                                <!-- <div class="author__text row">
-                                  <div class="col-lg-3 col-md-4">
-                                    <p class="mb-0">{{ $t("page_course_user_create") }}</p>
-                                  </div>
-                                  <div class="col-lg-9 col-md-8">
-                                    
-                                  </div>
-                                </div> -->
+                               
                                 <div class="author__text row">
                                   <div class="col-lg-3 col-md-4">
-                                    <p class="mb-0">{{ $t("page_course_date") }}</p>
+                                    <p class="mb-0">
+                                      {{ $t("page_course_date") }}
+                                    </p>
                                   </div>
                                   <div class="col-lg-9 col-md-8">
-                                    <p class="mb-0"> {{ coverttime(store.data.crt_date) }}
+                                    <p class="mb-0">
+                                      {{ coverttime(store.data.crt_date) }}
                                     </p>
                                   </div>
                                 </div>
-<!-- 
-                                       <div class="author__text row">
-                                  <div class="col-lg-3 col-md-4">
-                                    <p class="mb-0">{{ $t("page_course_last_description") }}</p>
-                                  </div>
-                                  <div class="col-lg-9 col-md-8">
-                                   {{store.data.cs_description}}
-                                  </div>
-                                </div> -->
+                        
                               </div>
                             </div>
                           </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div
+                  class="tab-content tab__content__wrapper"
+                  id="myTabContent"
+                >
+                  <div
+                    class="tab-pane fade active show"
+                    id="projects__two"
+                    role="tabpanel"
+                    aria-labelledby="projects__two"
+                  >
+                    <div
+                      class="accordion content__cirriculum__wrap"
+                      id="accordionExample"
+                    >
+                      <div class="accordion-item">
+                      
+                        <div
+                          id="collapseOne"
+                          class="accordion-collapse collapse p-4 show"
+                          aria-labelledby="headingOne"
+                          data-bs-parent="#accordionExample"
+                        >
+                          <div
+                            class="row justify-content-center"
+                            v-if="store.data.cs_video"
+                          >
+                            <div v-if="store.isYoutube == true" class="ifra">
+                              <YouTube
+                                :src="store.data.cs_video"
+                                :width="store.windowWidth"
+                                @ready="onReady"
+                                @playing="handlePlaying"
+                                ref="youtube"
+                              />
+                            </div>
+                          </div>
+
+                
+                        
                         </div>
                       </div>
                     </div>
@@ -213,7 +174,7 @@
     </div>
   </div>
 
-    <div v-else>
+  <div v-else>
     <div class="coursearea sp_bottom_100">
       <div class="container">
         <div class="row" v-if="store.data">
@@ -230,14 +191,12 @@
                 data-aos="fade-up"
               >
                 <div class="course__button"></div>
-            
               </div>
-      
+
               <div
                 class="course__details__tab__wrapper aos-init"
                 data-aos="fade-up"
               >
-              
                 <div
                   class="tab-content tab__content__wrapper"
                   id="myTabContent"
@@ -247,9 +206,7 @@
                     id="projects__two"
                     role="tabpanel"
                     aria-labelledby="projects__two"
-                 
                   >
-           
                     <div
                       class="accordion content__cirriculum__wrap"
                       id="accordionExample"
@@ -258,10 +215,10 @@
                         <h2 class="accordion-header" id="headingOne">
                           <button
                             class="accordion-button collapsed mt-0"
-                            type="button" style="background-color:#7e7ec3"
-                        
+                            type="button"
+                            style="background-color: #7e7ec3"
                           >
-                           {{store.data.cs_name}}
+                            {{ store.data.cs_name }}
                           </button>
                         </h2>
                         <div
@@ -270,14 +227,10 @@
                           aria-labelledby="headingOne"
                           data-bs-parent="#accordionExample"
                         >
-                     
-
                           <div class="row" style="padding: 5px">
                             <div
                               class="col-lg-3 d-flex justify-content-center align-items-center"
-                            >
-                            
-                            </div>
+                            ></div>
                             <div class="col-lg-9 col--30">
                               <div
                                 class="author__content"
@@ -286,22 +239,14 @@
                                 <div class="author__text row">
                                   <div class="col-lg-3 col-md-4">
                                     <p class="fs-4 fw-bold mb-0">
-                                     ไม่มีบทเรียนนี้
+                                      ไม่มีบทเรียนนี้
                                     </p>
                                   </div>
                                   <div class="col-lg-9 col-md-8">
-                                    <p class="fs-4 fw-bold mb-0">
-                                     
-                                    </p>
+                                    <p class="fs-4 fw-bold mb-0"></p>
                                   </div>
                                 </div>
-                        
-                           
-                            
-                          
-
-                                
-                              </div> 
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -324,29 +269,39 @@
       </h1>
     </div> -->
     <div class="accordion accordion-flush" id="accordionFlushExample">
-   
       <div class="accordion-item">
         <h2 class="accordion-header">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-            Q1:  {{store.data.cs_name}} ?
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#flush-collapseThree"
+            aria-expanded="false"
+            aria-controls="flush-collapseThree"
+          >
+            Q1: {{ store.data.cs_name }} ?
           </button>
         </h2>
-        <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-          <div class="accordion-body">{{store.data.cs_description}}</div>
+        <div
+          id="flush-collapseThree"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionFlushExample"
+        >
+          <div class="accordion-body">{{ store.data.cs_description }}</div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-    definePageMeta({
-  middleware: 'auth' // this should match the name of the file inside the middleware directory 
-})
-import { defineComponent, ref, onMounted, onBeforeUnmount  } from 'vue';
+definePageMeta({
+  middleware: "auth", // this should match the name of the file inside the middleware directory
+});
+import { defineComponent, ref, onMounted, onBeforeUnmount } from "vue";
 import { storeToRefs } from "pinia";
 import { LessonStore } from "@/stores/lesson";
 import ApiService from "@/services/api.service";
-import YouTube from 'vue3-youtube'
+import YouTube from "vue3-youtube";
 const router = useRouter();
 const store = LessonStore();
 const youtubeWidth = ref(620);
@@ -358,68 +313,54 @@ store.user_id = auth.user_id;
 
 let lesson_id = await store.fetchLessonId(router.currentRoute.value.params.id);
 
-
-if(lesson_id == true){
-   store.updateLogCourse();
-}else {
-
+if (lesson_id == true) {
+  store.updateLogCourse();
+} else {
 }
 if (process.client) {
-
   window.addEventListener("resize", () => loadEdit(), store.isYoutube == false);
-  
-//   window.addEventListener('resize', function(event) {
-  
-//     updateWidth();
-// }, true);
 
+  //   window.addEventListener('resize', function(event) {
+
+  //     updateWidth();
+  // }, true);
 }
 
 async function loadEdit() {
+  store.isYoutube = false;
 
- store.isYoutube = false;
+  setTimeout(function () {
+    updateWidth();
+  }, 1000); //delay is in milliseconds
 
-setTimeout(function(){
-          updateWidth();
-   },1000); //delay is in milliseconds 
-
- // await updateWidth();
- // console.log('loadEdit');
- 
+  // await updateWidth();
+  // console.log('loadEdit');
 }
 
-const handlePlaying = () => {
-    
-    };
+const handlePlaying = () => {};
 
-
-    
-  const updateWidth = async () => {
-
+const updateWidth = async () => {
   if (window.innerWidth < 768 && window.innerWidth >= 430) {
     youtubeWidth.value = 350;
     store.windowWidth = youtubeWidth.value;
-   store.isYoutube = true 
+    store.isYoutube = true;
   } else if (window.innerWidth < 430) {
     youtubeWidth.value = 250;
     store.windowWidth = youtubeWidth.value;
-    store.isYoutube = true 
-  }
-  else {
+    store.isYoutube = true;
+  } else {
     youtubeWidth.value = 620; // default width
     store.windowWidth = youtubeWidth.value;
-     store.isYoutube = true 
+    store.isYoutube = true;
   }
 };
 onMounted(() => {
-
- // window.addEventListener('resize', updateWidth);
-   updateWidth(); // Call once when component is mounted to set the initial width
+  // window.addEventListener('resize', updateWidth);
+  updateWidth(); // Call once when component is mounted to set the initial width
 });
 
 onBeforeUnmount(() => {
- 
- // window.removeEventListener('resize', updateWidth);
+  // window.removeEventListener('resize', updateWidth);
 });
 
 let youtube = "https //www.youtube.com/embed/tgbnymz7vqy";
@@ -430,20 +371,15 @@ const options = reactive({
   poster: "", //封面
 });
 const onPlay = (id) => {
-
-//  store.cs_id = id;
-// store.updateLogCourse();
+  //  store.cs_id = id;
+  // store.updateLogCourse();
 };
 
 const backlearning = (couse_id) => {
-   router.push("/course-detail/"+couse_id);
+  router.push("/course-detail/" + couse_id);
 };
 
-
-const onReady = () => {
-
-};
-
+const onReady = () => {};
 
 function coverimage(i) {
   let im = ApiService.image(i);
@@ -469,6 +405,11 @@ function coverttime(date) {
 .blogarae__img__2 > img {
   max-height: 430cm;
   object-fit: cover;
+}
+
+.responsive {
+  width: 100%;
+  height: auto;
 }
 
 .containerxxx {
@@ -514,6 +455,6 @@ function coverttime(date) {
   overflow: hidden;
 }
 .ifra {
-text-align:-webkit-center;
+  text-align: -webkit-center;
 }
 </style>
