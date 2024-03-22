2d2ca01990e35b451cca0f839e7cc3a2ef45a9ca
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-03-22 10:54:01 +0700

    UpdateFooter

---
+++ components/layout/Footer2.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="footerarea">
+        <div class="container">
+            <div class="footerarea__wrapper footerarea__wrapper__2 py-5">
+                <div class="row justify-content-center">
+                    <div class="col-xl-5 col-lg-5 col-md-6 col-sm-12" data-aos="fade-up">
+                        <div class="footerarea__inner footerarea__about__us">
+                            <div class="footerarea__heading">
+                                <h3>ກົມຂົນສົ່ງ, ກະຊວງ​ໂຍທາທິການແລະ​ຂົນສົ່ງ</h3>
+                                <img src="logoPWT.png" alt="logo" width="120" />
+                            </div>
+                            <div class="footerarea__heading">
+                                <h3>Sponsored and Supported by:</h3>
+                                <div class="d-flex gap-4">
+                                    <img src="/img/logo/ursf2.jpg" width='110' alt="img" />
+                                    <img src="/img/logo/escap.png" width='130' alt="img" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-5 col-lg-5  col-md-6 col-sm-12 col-12" data-aos="fade-up">
+                        <div class="footerarea__inner">
+                            <div class="footerarea__heading">
+                                <h3>ຕິດ​ຕໍ່​ພວກ​ເຮົາ</h3>
+                            </div>
+                            <div class="footerarea__list">
+                                <ul>
+                                    <li>
+                                        <a href="#">Department of Transport, Lane Xang Avenue, Xaysettha district, Vientiane Capital, Lao PDR</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Tel: 856-21 412250</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Email: depttransportlaos@gmail.com</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icofont-google-map"></i> Open location on Map</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icofont-facebook"></i> Department of Transport Laos</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-2  col-md-4 col-sm-12" data-aos="fade-up">
+                        <div class="d-flex justify-content-center">
+                            <a href="" class="btn btn-success">Download App</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="background-color: var(--dotColor);">
+        <div class="container">
+            <div class="row py-3">
+                <div class="col-12 col-xl-7 col-lg-7 text-center text-lg-start">
+                    <p class="mb-0 text-dark">Copyright @ Department of Transport, Ministry of Public Works and Transport, Lao PDR</p>
+                </div>
+                <div class="col-12 col-xl-5 col-lg-5 text-center text-lg-end">
+                    <p class="mb-0 text-dark">Developed by: <a href="https://www.datacom.la/" target='_BLANK' className='text-primary'>Datacom Co.,Ltd</a> & <a href="https://iddrives.co.th/web/" className='text-primary' target='_BLANK'>ID Drives Co., Ltd</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
