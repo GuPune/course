030c0eb79f9b7fc85cfab188608d37b09e1ede86
iboonkhet <iboonkhet@gmail.com>
2024-04-29 16:07:06 +0700

    update
    update

--- app.vue
+++ app.vue
@@ -45,10 +45,10 @@ useHead({
   ],
   link: [
       { rel: 'icon', type: 'image/png', href: '/LogoPWT.png' },
-      // {
-      //   rel: 'stylesheet',
-      //   href: 'https://db.onlinewebfonts.com/c/938a9833b7244d806eca3349487c68ba?family=Phetsarath+OT',
-      // },
+      {
+        rel: 'stylesheet',
+        href: 'https://db.onlinewebfonts.com/c/938a9833b7244d806eca3349487c68ba?family=Phetsarath+OT',
+      },
       // You can add other link tags here
     ]
 })
